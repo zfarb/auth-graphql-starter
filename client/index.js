c9dfd64b04e6da1532ccefef9600ba34fe3c29ed
zacharyfarber <zachfarber001@icloud.com>
2023-05-28 18:12:53 -0400

    created App and Header components

--- client/index.js
+++ client/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, hashHistory, Route, indexRoute } from 'react-router';
+import App from './App';
 
 const client = new ApolloClient({
     dataIdFromObject: (o) => o.id
 });
 
 const Root = () => {
-    return <ApolloProvider client={client}>Auth Starter</ApolloProvider>;
+    return (
+        <ApolloProvider client={client}>
+            <Router history={hashHistory}>
+                <Route path="/" component={App}></Route>
+            </Router>
+        </ApolloProvider>
+    );
 };
 
 ReactDOM.render(<Root />, document.querySelector('#root'));
