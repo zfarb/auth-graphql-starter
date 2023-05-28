183f1855f079b8b16810601a6f63fcc957048685
zacharyfarber <zachfarber001@icloud.com>
2023-05-28 19:06:46 -0400

    created LoginForm and AuthForm

---
+++ client/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <div>
+                <AuthForm />
+            </div>
+        );
+    }
+}
+
+export default LoginForm;
