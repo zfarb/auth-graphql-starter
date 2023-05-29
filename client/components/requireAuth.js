f49223454e0fa560cfc78e38081200c57e47d412
zacharyfarber <zachfarber001@icloud.com>
2023-05-28 23:54:59 -0400

    added a requireAuth HOC to the Dashboard component

---
+++ client/components/requireAuth.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import query from '../queries/currentUser';
+import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
+
+export default (WrappedComponent) => {
+    class RequireAuth extends Component {
+        componentWillUpdate(nextProps) {
+            if (!nextProps.data.loading && !nextProps.data.user) {
+                hashHistory.push('/login');
+            }
+        }
+
+        render() {
+            return <WrappedComponent {...this.props} />;
+        }
+    }
+
+    return graphql(query)(RequireAuth);
+};
