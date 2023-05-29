246c96379290f72c1e2a6616b2f3baa55c0d2033
zacharyfarber <zachfarber001@icloud.com>
2023-05-28 23:37:05 -0400

    added SignupForm error handling and redirect

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -3,25 +3,45 @@ import AuthForm from './AuthForm';
 import mutation from '../mutations/signup';
 import query from '../queries/currentUser';
 import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
 
 class SignupForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { errors: [] };
+    }
+
+    componentWillUpdate(nextProps) {
+        if (!this.props.data.user && nextProps.data.user) {
+            hashHistory.push('/dashboard');
+        }
+    }
+
     onSubmit({ email, password }) {
         const { mutate } = this.props;
 
         mutate({
             variables: { email, password },
             refetchQueries: [{ query }]
-        });
+        }).catch((res) =>
+            this.setState({
+                errors: res.graphQLErrors.map((err) => err.message)
+            })
+        );
     }
 
     render() {
         return (
             <div>
                 <h3>Sign Up</h3>
-                <AuthForm onSubmit={this.onSubmit.bind(this)} />
+                <AuthForm
+                    onSubmit={this.onSubmit.bind(this)}
+                    errors={this.state.errors}
+                />
             </div>
         );
     }
 }
 
-export default graphql(mutation)(SignupForm);
+export default graphql(query)(graphql(mutation)(SignupForm));
